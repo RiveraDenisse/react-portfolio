fdc214319850643f0b1f1ea6a26b048989e9e3a5
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-09-23 23:41:08 -0500

    modified about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,16 @@ import React from "react";
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">here is my about info</h1>
+      <p id="about">
+        I am an electrical engineer (with a computer engineering emphasis)
+        currently working for an electric utility. After 10 years of working
+        with utilities and consultants I decided it was time to go back to
+        computers, so I enrolled on a programming bootcamp to learn and get some
+        programming experience. I am currently learning HTML,CSS,
+        Javascript,Node.js, React, MongoDB among others. My spare time consists
+        of playing video games, watching series/movies and play with my three
+        dogs.
+      </p>
     </section>
   );
 }
